d34a43a079c11fd70d70b296714298ac8225b3f3
Taha Salman <taha.s147@gmail.com>
2019-12-07 03:04:22 -0500

    added tls_rsa_with_aes_128_cbc decryption

--- config-sample.py
+++ config-sample.py
@@ -1,2 +1,5 @@
+PRIVATE_KEY_FILE = "keys/privkey.pem"
+PUBLIC_KEY_FILE = "keys/pubkey.pem"
+
 PUBLIC_KEY = ""
 PRIVATE_KEY = ""
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import serialization, hashes, hmac
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
-
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import config
+
+from decryptor import TLS_Decryptor
 
 
 def read_private_key(filename, password=None):
@@ -37,30 +39,113 @@ def load_public_key():
     )
     return public_key
 
+
+def PRF(secret, label, seed, length=48):
+    seed = label + seed
+    result = b""
+
+    h = hmac.HMAC(secret, hashes.SHA256(), backend=default_backend())
+    a = seed
+    while len(result) < length:
+        h.update(a)
+        a = h.copy().finalize()
+        h.update(a+seed)
+        p = h.copy().finalize()
+        result += p
+    return result
+
+    #
+    # a0 = seed
+    # a1 = hmac.new(secret, a0, hashlib.sha256)
+    # a2 = hmac.new(secret, a1, hashlib.sha256)
+    # p1 = hmac.new(secret, a1+seed, hashlib.sha256)
+    # p2 = hmac.new(secret, a2+seed, hashlib.sha256)
+    # return (p1 + p2[:16])
+
 def test():
     private_key = read_private_key("keys/privkey.pem")
     public_key = read_public_key("keys/pubkey.pem")
 
-    print(private_key)
-    print(public_key)
+    client_random = config.CLIENT_RANDOM
+    server_random = config.SERVER_RANDOM
 
 
     premaster_secret = config.ENCRYPTED_PREMASTER_SECRET
     print(len(premaster_secret))
 
-    plaintext = private_key.decrypt(
+    premaster_secret = private_key.decrypt(
         premaster_secret,
         padding.PKCS1v15()
     )
 
-    print(plaintext)
-    print(len(plaintext))
+    print(premaster_secret)
+    print(len(premaster_secret))
+
+    master_secret = PRF(premaster_secret,
+                        b"master secret",
+                        client_random + server_random)[:48]
+    print(master_secret)
+    print(len(master_secret))
+
+    ## Generate key block to derive keys from
+    MAC_KEY_LENGTH = 20
+    SYMMETRIC_KEY_LENGTH = 16
+    IV_LENGTH = 16
+
+    key_block = PRF(master_secret,
+                    b'key expansion',
+                    server_random + client_random,
+                    (2*MAC_KEY_LENGTH + 2*SYMMETRIC_KEY_LENGTH + 2*IV_LENGTH ))
+
+    print(f'KEY BLOCK\n\n {key_block}')
+    print(f'LENGTH: {len(key_block)}')
+    client_write_mac = key_block[:MAC_KEY_LENGTH]
+    key_block = key_block[MAC_KEY_LENGTH:]
+    server_write_mac = key_block[:MAC_KEY_LENGTH]
+    key_block = key_block[MAC_KEY_LENGTH:]
+    client_write_key = key_block[:SYMMETRIC_KEY_LENGTH]
+    key_block = key_block[SYMMETRIC_KEY_LENGTH:]
+    server_write_key = key_block[:SYMMETRIC_KEY_LENGTH]
+    key_block = key_block[SYMMETRIC_KEY_LENGTH:]
+    client_write_IV = key_block[:IV_LENGTH]
+    key_block = key_block[IV_LENGTH:]
+    server_write_IV = key_block[:IV_LENGTH]
+
+    print(client_write_key)
+    print(len(client_write_key))
+    print(len(server_write_IV))
+
+
+    ## Now we do AES_128_CBC Decryption
+
+    # Decrypt Client Data
+    client_cipher = Cipher(algorithm=algorithms.AES(client_write_key),
+                    mode=modes.CBC(client_write_IV),
+                    backend=default_backend())
+    client_decryptor = client_cipher.decryptor()
+    client_decrypted_data = client_decryptor.update(config.CLIENT_ENCRYPTED_DATA) + client_decryptor.finalize()
+
+    print(client_decrypted_data)
+    print(client_decrypted_data.hex())
+    print(len(client_decrypted_data))
+    # Decrypt Server Data
+
+
+
 
 def main():
-    private_key = load_private_key()
-    public_key = load_public_key()
-    print(public_key)
-    print(private_key)
+    decryptor = TLS_Decryptor(
+        client_random=config.CLIENT_RANDOM,
+        server_random=config.SERVER_RANDOM,
+        enc_pre_master_secret=config.ENCRYPTED_PREMASTER_SECRET,
+        mac_key_length=20,
+        key_length=16,
+        iv_length=16
+    )
+
+    decrypted_client_data = decryptor.decrypt_client_data(config.CLIENT_ENCRYPTED_APP_DATA)
+    print(decrypted_client_data.hex())
+    print(len(decrypted_client_data))
 
 if __name__ == "__main__":
-    test()
+    main()
